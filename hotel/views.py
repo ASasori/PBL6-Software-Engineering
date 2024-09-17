306ccd0e596acecbb5b6f7b945957e08041d90ef
ASasori <93644975+ASasori@users.noreply.github.com>
2024-09-17 08:02:34 +0700

    merge 1

--- hotel/views.py
+++ hotel/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from hotel.models import  Hotel, Room, Booking, HotelGallery, HotelFeatures, RoomType
 # Create your views here.
 def index(request):
-    return render(request, "hotel/hotel.html")
+    hotels = Hotel.objects.filter(status="Live")
+    context = {
+        "hotel":hotels
+    }
+    return render(request, "hotel/hotel.html", context)
+
