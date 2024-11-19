2de17fedc7d5748320304ed96b3bb754a9742199
ASasori <93644975+ASasori@users.noreply.github.com>
2024-11-19 13:19:26 +0700

    Change mysql version6

--- hms_prj/settings.py
+++ hms_prj/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-&8$zx(^(s0nmnx4k-+4g2&*=nh7ox1e^jxu905v)h#^h@z&z!0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['103.78.0.191', 'wireguard.nguyentanloc.top', '127.0.0.1', 'localhost']
 SECURE_CROSS_ORIGIN_OPENER_POLICY='same-origin-allow-popups'
 
 # Application definition
