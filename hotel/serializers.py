7ffcc6b0763bc1a41eab33adf0772ee9e3fcdc63
ASasori <93644975+ASasori@users.noreply.github.com>
2024-11-22 15:18:52 +0700

    hotel gallery api

--- hotel/serializers.py
+++ hotel/serializers.py
@@ -1,10 +1,23 @@
 from rest_framework import serializers
-from .models import Hotel, RoomType, Room, Booking, Cart, CartItem
+from .models import Hotel, RoomType, Room, Booking, Cart, CartItem, HotelGallery
+from taggit.serializers import TagListSerializerField
+
+class HotelGallerySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HotelGallery
+        fields = ['image']  # Add fields you want to expose
+
 
 class HotelSerializer(serializers.ModelSerializer):
+    hotel_gallery = HotelGallerySerializer(many=True, read_only=True) 
+    tags = TagListSerializerField()
     class Meta:
         model = Hotel
-        fields = '__all__'
+        fields = [
+            'user', 'name', 'description', 'map_image', 'address', 'mobile',
+            'email', 'status', 'tags', 'views', 'featured', 'hid', 'slug',
+            'date', 'hotel_gallery'  # Add hotel_gallery to fields
+        ]
 
 class RoomTypeSerializer(serializers.ModelSerializer):
     class Meta:
