ebf7aca6cd2efb48c5ca94fc5e777683a57e9edc
ASasori <93644975+ASasori@users.noreply.github.com>
2024-12-11 22:43:34 +0700

    fix for deploy 2

--- hms_prj/settings.py
+++ hms_prj/settings.py
@@ -84,6 +84,8 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # Thay đổi thành miền frontend của bạn
+    "https://hotel-management-fe-omega.vercel.app",  # Địa chỉ của ứng dụng React
+    "https://pbl6-receptionist.netlify.app",
 ]
 CORS_ALLOW_HEADERS = list(default_headers) + [
     'Server-Domain',
@@ -95,19 +97,19 @@ ROOT_URLCONF = 'hms_prj.urls'
 
 CSRF_COOKIE_NAME = 'csrftoken'
 CSRF_COOKIE_HTTPONLY = False  
-CSRF_COOKIE_SECURE = True    
+# CSRF_COOKIE_SECURE = True    
 CSRF_COOKIE_SAMESITE = 'Lax' 
-SESSION_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:5173',
 ]
 
-SECURE_HSTS_SECONDS = 31536000  # 1 year
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
+# SECURE_HSTS_SECONDS = 31536000  # 1 year
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_HSTS_PRELOAD = True
 
-SECURE_SSL_REDIRECT = True
+# SECURE_SSL_REDIRECT = True
 
 TEMPLATES = [
     {
