d78a603b70775686845971f4b3a8bca0e579784e
ASasori <93644975+ASasori@users.noreply.github.com>
2024-12-11 15:07:05 +0700

    resolve conflict

--- hms_prj/urls.py
+++ hms_prj/urls.py
@@ -56,10 +56,7 @@ urlpatterns = [
     #API document
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-<<<<<<< HEAD
 
-=======
->>>>>>> 31ff11e8338b3f14017d20e895aa84a438088467
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  
